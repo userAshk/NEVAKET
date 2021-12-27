138d1da73fe9e0c242a0f377c5a008ad06fc777c
userAshk <blvckm8@gmail.com>
2021-12-27 22:36:06 +0300

    responsive v

--- js/app.js
+++ js/app.js
@@ -1,12 +1,64 @@
-//BURGER MENU SHOW HIDE
+//BURGER MENU 
 $('.menu-btn').on('click', function(e){
     e.preventDefault;
     $(this).toggleClass('menu-btn_active');
     $('.menu-block').toggleClass('menu-block_active')
-    
   
     $('.menu-block a' ).on('click', function(e){
       $('.menu-block').removeClass('menu-block_active')
       $('.menu-btn').removeClass('menu-btn_active')
+
     })  
+
+});
+
+
+var className = "inverted";
+var scrollTrigger = 60;
+
+window.onscroll = function() {
+  if (window.scrollY >= scrollTrigger || window.pageYOffset >= scrollTrigger) {
+    document.getElementsByTagName("header")[0].classList.add('header-bg');
+  } else {
+    document.getElementsByTagName("header")[0].classList.remove('header-bg');
+  }
+};
+
+
+$('.carousel').slick({
+  centerMode: true,
+  dots: true,
+  centerPadding: '60px',
+  slidesToShow: 1,
+  adaptiveHeight: true,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    }
+  ]
+});
+
+
+var map;
+
+  DG.then(function () {
+    map = DG.map('map', {
+    center: [42.87, 74.58],
+    zoom: 13
+  });
 });
